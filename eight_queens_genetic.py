1022e9088ee5dc7e443b9b9ad2473d0170b518fa
McKennon McMillian <mbmcmill@go.olemiss.edu>
2018-04-03 12:11:17 -0500

    Genetic works

--- eight_queens_genetic.py
+++ eight_queens_genetic.py
@@ -1,10 +1,11 @@
 import random
 from operator import itemgetter
 from tqdm import tqdm
-n = 8
+N = 8
 win = 0
 stop = False
-c = 0
+C = 0
+REPS = 1000
 def conflicts(s):
 	h = 0
 	for i in range(len(s)):
@@ -21,20 +22,42 @@ def conflicts(s):
 	return h
 
 def generate_population(p_size):
-	population = [[random.randint(0, 7) for x in range(n)] for i in range(p_size)]
+	population = [[random.randint(0, 7) for x in range(N)] for i in range(p_size)]
 	for i in range(p_size):
 		population[i] = [population[i], conflicts(population[i])]
 	return sorted(population, key=itemgetter(1))
+
+def kinda_random(population):
+	best = []
+	p = population.copy()
+	p = p[:25]
+	o = random.randint(0, len(p)-1)
+	best.append(p[0])
+	del p[0]
+	best.append(p[random.randint(0, len(p)-1)])
+	return best
+
+# def kinda_random_two(population):
+# 	best = []
+# 	p = population.copy()
+# 	s = 0
+# 	for i in range(len(p)):
+# 		s += p[i][1]
+# 	for i in range(len(p)):
+		
+
+# 	return best
 
 
 def crossover(pop, m):
 	global stop
 	global win
-	global c
-	for i in range(0,len(pop),2):
+	global C
+	for i in range(0,len(pop)):
 		cross = random.randint(1, 7)
-		one = pop[random.randint(0, len(pop)-1)]
-		two = pop[random.randint(0, len(pop)-1)]
+		ran = kinda_random(pop)
+		one = ran[0]
+		two = ran[1]
 		check_h = min(one[1],two[1])
 		front_one = one[0][:cross]
 		if random.uniform(0, 1) < m:
@@ -54,22 +77,22 @@ def crossover(pop, m):
 			end_two[mut] = random.randint(0, 7)
 		pop.append([front_one+end_two, conflicts(front_one+end_two)])
 		pop.append([front_two+end_one, conflicts(front_two+end_one)])
-		c += 1
+		C += 1
 		if check_h == 0 or conflicts(front_one+end_two) == 0 or conflicts(front_two+end_one) == 0:
 			stop = True
 			win += 1
 			break
-		if c == 50:
+		if C == 2000:
 			stop = True
-			c = 0
+			C = 0
 			break
 	# pop = kill_em(pop)
 	return sorted(pop, key=itemgetter(1))
 
 
 mutation_rate = .2
-for x in tqdm(range(1000)):
-	starting_population = generate_population(1000)
+for x in tqdm(range(REPS)):
+	starting_population = generate_population(100)
 	pop = crossover(starting_population, mutation_rate)
 	while not stop:
 		pop = crossover(pop, mutation_rate)
